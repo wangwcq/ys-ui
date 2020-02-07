6564a06f073007bbad60c749da428ef18efdf45d
Chris Wang <chris.wang@mioying.com>
2020-02-08 01:42:05 +0800

    v2.1.1

---
+++ components/UiContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "UiContainer"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/UiLogo.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "UiLogo"
+  }
+</script>
+
+<style scoped>
+
+</style>
