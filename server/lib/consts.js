1bc439caefa86e4ee3954745c424e2039bbaddcf
Chris Wang <chris.wang@mioying.com>
2020-04-10 07:05:04 +0800

    Feature: Add time interval consts to server package

--- server/lib/consts.js
+++ server/lib/consts.js
@@ -1,6 +1,18 @@
+const SECONDS = 1000;
+const MINUTES = 60 * SECONDS;
+const HOURS = 60 * MINUTES;
+const DAYS = 24 * HOURS;
+const timeIntervalConsts = {
+  SECONDS,
+  MINUTES,
+  HOURS,
+  DAYS,
+};
+
 const consts = {
   parallelSize: 5,
   env: process.env || {},
+  timeIntervalConsts,
 };
 
 module.exports = consts;
